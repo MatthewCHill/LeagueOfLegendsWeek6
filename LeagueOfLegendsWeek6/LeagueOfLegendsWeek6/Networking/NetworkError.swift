4e6543965833e072987716b6d6df8b94a82f895f
Matthew Hill <matthill265@gmail.com>
2023-03-11 18:01:48 -0700

    Creates the NetworkError file to use custom errors for better descriptions

---
+++ LeagueOfLegendsWeek6/LeagueOfLegendsWeek6/Networking/NetworkError.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkError.swift
+//  LeagueOfLegendsWeek6
+//
+//  Created by Matthew Hill on 3/11/23.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case invalidURL
+    case thrownError(Error)
+    case noData
+    case unableToDecode
+    case emptyArray
+    case invalidStatusCode
+    
+    var errorDescription: String? {
+        switch self {
+            
+        case .invalidURL:
+            return "Invalid URL. Check URL endpoint."
+        case .thrownError(let error):
+            return "Thrown error. Error was \(error.localizedDescription)"
+        case .noData:
+            return "No data received from the network fetch."
+        case .unableToDecode:
+            return "Unable to decode model object from data."
+        case .emptyArray:
+            return "Fetch returned an empty array."
+        case .invalidStatusCode:
+            return "Fetch returned an unsuccessful status code. Code was not 200."
+        }
+    }
+}
